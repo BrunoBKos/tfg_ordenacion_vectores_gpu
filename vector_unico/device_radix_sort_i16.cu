be8bfbcc5d02a8fe56c4b13b0591b7df04655a4b
BrunoBKos <125130715+BrunoBKos@users.noreply.github.com>
2025-12-26 21:44:31 +0100

    first commit

---
+++ vector_unico/device_radix_sort_i16.cu
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cub/cub.cuh>
+
+int N = 1024;
+
+__global__ void emptyKernel() {}
+
+// main function
+int main(int argc, char** argv) {
+
+  if(argc > 1) N = atoi(argv[1]);
+
+  // host vectors
+  short* vect;
+  short* sorted_vector;
+
+  // device vectors
+  short* d_vector;
+  short* d_sorted_vector;
+
+  // temp variables
+  struct timeval tv_start, tv_end;
+  double run_time;
+
+  // memmory reserve in CPU
+  vect = (short*) malloc(N*sizeof(short));
+  sorted_vector = (short*) malloc(N*sizeof(short));
+
+  // memory reserve in GPU
+  cudaMalloc(&d_vector, N*sizeof(short));
+  cudaMalloc(&d_sorted_vector, N*sizeof(short));
+
+  emptyKernel<<<1, 1>>>(); // GPU warmup
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  // Determine temporary device storage requirements
+  void* d_temp_storage = NULL;
+  size_t temp_storage_bytes = 0;  
+  cub::DeviceRadixSort::SortKeys(d_temp_storage, temp_storage_bytes, d_vector, d_sorted_vector, N); 
+  // Allocate temporary storage
+  cudaMalloc(&d_temp_storage, temp_storage_bytes);
+
+  // initialization
+  srand((unsigned int) time(NULL));
+  for(int i = 0; i < N; i++) vect[i]=((short) rand()%1000);
+  
+  cudaDeviceSynchronize();
+  
+  gettimeofday(&tv_start, NULL);
+
+  //device copy
+  cudaMemcpyAsync(d_vector, vect, N*sizeof(short), cudaMemcpyHostToDevice, stream);
+  // Run sorting operation
+  cub::DeviceRadixSort::SortKeys(d_temp_storage, temp_storage_bytes, d_vector, d_sorted_vector, N); 
+  // recovery of the parallel results
+  cudaMemcpyAsync(sorted_vector, d_sorted_vector, N*sizeof(short), cudaMemcpyDeviceToHost, stream);
+  cudaStreamSynchronize(stream);
+
+  gettimeofday(&tv_end, NULL);
+  run_time=(tv_end.tv_sec - tv_start.tv_sec) * 1000000 + (tv_end.tv_usec - tv_start.tv_usec); //useconds
+  run_time /= 1000000; // seconds
+  printf("Tiempo: %lg segundos\n", run_time);
+
+  // free host memory
+  free(vect);
+  free(sorted_vector);
+
+  // free device memory
+  cudaFree(d_vector);
+  cudaFree(d_sorted_vector);
+  cudaFree(d_temp_storage);
+  cudaStreamDestroy(stream);
+
+  return 0;
+  
+}
+
+
---
+++ vector_unico/merge_sort_f64.cu
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cub/cub.cuh>
+
+int N = 1024;
+
+__global__ void emptyKernel() {}
+
+struct CustomLess
+{
+  template <typename DataType>
+  __device__ bool operator()(const DataType &lhs, const DataType &rhs)
+  {
+    return lhs < rhs;
+  }
+};
+
+// main function
+int main(int argc, char** argv) {
+
+  if(argc > 1) N = atoi(argv[1]);
+
+  // host vectors
+  double* vect;
+  double* sorted_vector;
+
+  // device vectors
+  double* d_vector;
+
+  // temp variables
+  struct timeval tv_start, tv_end;
+  double run_time;
+
+  // memmory reserve in CPU
+  vect = (double*) malloc(N*sizeof(double));
+  sorted_vector = (double*) malloc(N*sizeof(double));
+
+  // memory reserve in GPU
+  cudaMalloc(&d_vector, N*sizeof(double));
+
+  emptyKernel<<<1, 1>>>(); // GPU warmup
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  // Determine temporary device storage requirements
+  void* d_temp_storage = NULL;
+  size_t temp_storage_bytes = 0;  
+  cub::DeviceMergeSort::SortKeys(d_temp_storage, temp_storage_bytes, 
+                                    d_vector, N, CustomLess(), stream);  
+  // Allocate temporary storage
+  cudaMalloc(&d_temp_storage, temp_storage_bytes);
+
+  // initialization
+  srand((unsigned int) time(NULL));
+  for(int i = 0; i < N; i++) vect[i]=((double)rand()/(double) RAND_MAX);
+
+  cudaDeviceSynchronize();
+    
+  gettimeofday(&tv_start, NULL);
+
+  //device copy
+  cudaMemcpyAsync(d_vector, vect, N*sizeof(double), cudaMemcpyHostToDevice, stream);
+  // Run sorting operation
+  cub::DeviceMergeSort::SortKeys(d_temp_storage, temp_storage_bytes, 
+                                    d_vector, N, CustomLess(), stream); 
+  // recovery of the parallel results
+  cudaMemcpyAsync(sorted_vector, d_vector, N*sizeof(double), cudaMemcpyDeviceToHost, stream);
+  
+  cudaStreamSynchronize(stream);
+
+  gettimeofday(&tv_end, NULL);
+  run_time=(tv_end.tv_sec - tv_start.tv_sec) * 1000000 + (tv_end.tv_usec - tv_start.tv_usec); //useconds
+  run_time /= 1000000; // seconds
+  printf("Tiempo: %lg segundos\n", run_time);
+
+  // free host memory
+  free(vect);
+  free(sorted_vector);
+
+  // free device memory
+  cudaFree(d_vector);
+  cudaFree(d_temp_storage);
+  cudaStreamDestroy(stream);
+  
+  return 0;
+  
+}
+
+
